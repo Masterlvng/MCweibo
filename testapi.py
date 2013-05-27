89503b8a1e95f13619f10db3337d3d008a456a73
cathvoilet@gmail.com <catherine@ubuntu.(none)>
2013-05-28 01:25:12 +0800

    third commit, to solve continuous posts

--- testapi.py
+++ testapi.py
@@ -3,7 +3,9 @@ import sys
 import weibo
 import webbrowser
 import json
- 
+import time 
+
+
 APP_KEY = '3586469417'
 MY_APP_SECRET = 'a5a5c8f8797979119e259d199012f046'
 REDIRECT_URL = 'https://api.weibo.com/oauth2/default.html'
@@ -15,7 +17,11 @@ expires_in = 1527353899
  
 client.set_access_token(access_token, expires_in)
 
-print expires_in
-client.statuses.update.post(status=u'hello, i mean no harm')
+client.statuses.update.post(status=u'tomorrow is annother day')
+time.sleep(15)
+client.statuses.update.post(status=u'to be or not to be')
+time.sleep(15)
+client.statuses.update.post(status=u'is it tough to make a decision like this?')
+
 print "*************OK**********"
 
