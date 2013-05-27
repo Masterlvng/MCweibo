9e85f0ae48cee5005d3f2a358fc3ba60eba738a8
cathvoilet@gmail.com <catherine@ubuntu.(none)>
2013-05-28 01:07:56 +0800

    second commit, solving token and expires_in

--- testapi.py
+++ testapi.py
@@ -10,20 +10,12 @@ REDIRECT_URL = 'https://api.weibo.com/oauth2/default.html'
  
 client = weibo.APIClient(APP_KEY, MY_APP_SECRET)
  
-authorize_url = client.get_authorize_url(REDIRECT_URL)
-
-webbrowser.open(authorize_url)
- 
-code = raw_input("input the code: ").strip()
- 
-request = client.request_access_token(code, REDIRECT_URL)
- 
-access_token = request.access_token
-expires_in = request.expires_in
-uid = request.uid
+access_token = '2.00d_KLoDvW9iuD4f32be1132s9YmnC'
+expires_in = 1527353899
  
 client.set_access_token(access_token, expires_in)
 
-client.statuses.update.post(status=u'I am a robot, put up your hands!')
+print expires_in
+client.statuses.update.post(status=u'hello, i mean no harm')
 print "*************OK**********"
 
